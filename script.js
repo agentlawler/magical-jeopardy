963ce6b6cb79bf9027fb97b4d2a6190b2f227fa2
Lindsey Lawler <lawlerlindsey@gmail.com>
2022-10-14 15:11:07 -0400

    html was way too bloated, after looking around for better css formatting, found a way to format html better. Back to square 1

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+
+//Sources used so far
+    //https://css-tricks.com/perfect-full-page-background-image/
+    //https://www.w3schools.com/cssref/
+    //https://www.youtube.com/watch?v=fYMBkayHmUo
+    //https://fontmeme.com/jeopardy-font/
+    
